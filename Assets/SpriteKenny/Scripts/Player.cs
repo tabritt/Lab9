acc50638f9636c7d501508a17dea3e71da63ddd0
Tiger <135745508+tabritt@users.noreply.github.com>
2025-10-15 14:01:24 -0400

    Finish at home

---
+++ Assets/SpriteKenny/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //injectoranywhere to gameobject the player
+
+    //
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Lab9
+How the Object Pooler is implemented
 
+How Builder Pattern 
+
+How Observer Pattern for Score System
