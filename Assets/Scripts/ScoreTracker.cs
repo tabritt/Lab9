841e67241403e977cb895427a15878091b87a021
tkpleo <tkristofferpurdy@gmail.com>
2025-10-19 22:57:34 -0400

    score UI element working

--- Assets/Scripts/ScoreTracker.cs
+++ Assets/Scripts/ScoreTracker.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Runtime.CompilerServices;
 
 public class ScoreTracker : MonoBehaviour
 {
     private int scoreCount = 0;
+    public TMP_Text scoreText;
 
     private void Start()
     {
         Enemy.onHit += ScoreUpdate;
     }
 
+    private void Update()
+    {
+        scoreText.text = "Score: " + scoreCount.ToString();
+    }
+
     // Update is called once per frame
     private void ScoreUpdate(int quantity)
     {
         scoreCount += quantity;
-        Debug.Log("Score: " + scoreCount);
+        Debug.Log("Current Score: " + scoreCount);
     }
 }
